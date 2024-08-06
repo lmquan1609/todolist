be3f4da7b8fffe4d08a1e30562ab661628896ad8
lmquan1609 <luuminhquan155@gmail.com>
2024-08-06 18:37:17 +0700

    init todo list

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"time"
+)
+
+type TodoItem struct {
+	Id          int        `json:"id"`
+	Title       string     `json:"title`
+	Description string     `json:"description"`
+	Status      string     `json:"status`
+	CreatedAt   *time.Time `json:"created_at"`
+	UpdatedAt   *time.Time `json:"updated_at,omitempty"`
+}
+
+func main() {
+	now := time.Now().UTC()
+	item := TodoItem{
+		Id:          1,
+		Title:       "Task 1",
+		Description: "Content 1",
+		Status:      "Doing",
+		CreatedAt:   &now,
+		UpdatedAt:   nil,
+	}
+
+	if jsData, err := json.Marshal(item); err != nil {
+		log.Fatalln(err)
+	} else {
+		log.Println(string(jsData))
+	}
+
+	jsStr := "{\"id\":1,\"Title\":\"Task 1\"}"
+
+	var item2 TodoItem
+	if err := json.Unmarshal([]byte(jsStr), &item2); err != nil {
+		log.Fatalln(err)
+	}
+	log.Println(item2)
+}
